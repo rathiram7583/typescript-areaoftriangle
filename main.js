ae18361e168d47c811b449141a368a90a982ba82
Rathi <rathiram7583@gmail.com>
2020-04-03 23:04:59 -0600

    Area

---
+++ main.js
@@ -0,0 +1,5 @@
+function areaoftriangle(base, height) {
+    var area = (base * height) / 2;
+    console.log("The area of the triangle :" + area);
+}
+areaoftriangle(5, 6);
---
+++ main.ts
@@ -0,0 +1,10 @@
+
+
+
+    function areaoftriangle( base:number,height:number){
+        var area=(base*height)/2;
+        console.log("The area of the triangle :"+area);
+    }
+
+
+areaoftriangle(5,6);
